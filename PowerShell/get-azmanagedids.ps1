698cb48d8cd89b58e86e632c78a3cf1ee0f324d8
Tim Sullivan <timothyj.sullivan1@gmail.com>
2019-05-15 09:38:33 -0500

    Added initial, rough managed IDs script

---
+++ PowerShell/get-azmanagedids.ps1
@@ -0,0 +1,44 @@
+Get-AzResourceGroup | % { 
+    $rg_name = $_.ResourceGroupName
+    Get-AzUserAssignedIdentity -ResourceGroupName $rg_name
+}
+
+
+#TODO: Add queries for Azure Functions
+#TODO: Add queries for Azure Logic Apps
+#TODO: Add queries for Azure Service Bus
+#TODO: Add queries for Azure Event Hubs
+#TODO: Add queries for Azure API Management
+#TODO: Add queries for Azure Container Instances
+
+get-azvm | % { 
+    $man_id = $_.identity.principalid
+    $name = $_.Name
+    $type = $_.type
+    $rg = $_.ResourceGroupName
+    
+    $obj = [PSCustomObject]@{
+        ResourceName = $name
+        ResourceGroup = $rg
+        ResourceType = $type
+        ManagedServiceID = $man_id
+    } 
+
+    write-output $obj
+}
+
+get-azwebapp | % { 
+    $man_id = $_.identity.principalid
+    $name = $_.Name
+    $type = $_.type
+    $rg = $_.ResourceGroup
+    
+    $obj = [PSCustomObject]@{
+        ResourceName = $name
+        ResourceGroup = $rg
+        ResourceType = $type
+        ManagedServiceID = $man_id
+    } 
+
+    write-output $obj
+}
