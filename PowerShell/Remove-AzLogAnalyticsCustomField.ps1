fc23dd0dad6f060021019af6aa22f882f2695151
Tim Sullivan <tisulliv@microsoft.com>
2019-12-04 17:28:01 -0600

    added powershell help

--- PowerShell/Remove-AzLogAnalyticsCustomField.ps1
+++ PowerShell/Remove-AzLogAnalyticsCustomField.ps1
@@ -1,6 +1,30 @@
-<#       
+<#
+.SYNOPSIS
+This cmdlet will help to remove custom fields from a log analytics workspace. This can be useful if you start to hit limits.
+
+.PARAMETER token
+An Azure Bearer token. To get this, run `az account get-access-token` from the Azure Cloud Shell (bash).
+
+.PARAMETER customField
+The name of the custom field. To get a list of all the fields, run the following query in Log Analytics:
+    AzureDiagnostics
+    | project data = pack_all(), ResourceProvider 
+    | mvexpand data limit 550
+    | parse tostring(data) with '{"' key : string '":' value : string '}'
+    | distinct key
+
+.PARAMETER workspace
+The workspace resourceID. To get this, run `(Get-AzOperationalInsightsWorkspace -Name $name -ResourceGroupName $rg).ResourceId` where $name is the name for your workspace and $rg is the resource group containing that workspace.
+
+.EXAMPLE
+
+.\Remove-AzLogAnalyticsCustomField.ps1 -token $token -customField 'BackupItemUniqueId_s' -workspace $workspace
+
+.NOTES
+
 Copyright (c) Tim Sullivan. All rights reserved.
 Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #>
 [CmdletBinding()]
 param (
