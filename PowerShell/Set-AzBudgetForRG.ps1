c2000f2a195d1e9ebd00e3dda327056574c357b4
Tim Sullivan <tisulliv@microsoft.com>
2020-01-13 14:07:59 -0600

    Added monthly costs array

--- PowerShell/Set-AzBudgetForRG.ps1
+++ PowerShell/Set-AzBudgetForRG.ps1
@@ -5,15 +5,21 @@ param (
     $ResourceGroupName
 )
 
+# Get the five most recent billing periods, but make sure they are in the past (-lt (get-date))
 $billing_periods = Get-AzBillingPeriod | sort-object -Top 5 | Where-Object BillingPeriodEndDate -lt (get-date) | Select-Object -ExpandProperty name
+$monthly_costs = @()
 
 foreach ($period in $billing_periods) 
 {
     Write-Verbose $period
     $summed_cost = Get-AzConsumptionUsageDetail -ResourceGroup $ResourceGroupName -BillingPeriodName $period | Measure-Object PretaxCost -Sum | Select-Object -ExpandProperty Sum
+
+    # We'll only take action if summed cost has a value. (Periods with no usage are $null, not 0)
     if ($summed_cost) 
     {
         Write-Verbose $summed_cost
+        $monthly_costs.Add($summed_cost)
     }       
 }
-    
+
+$monthly_costs | Sort-Object -Descending | Select-Object -First
