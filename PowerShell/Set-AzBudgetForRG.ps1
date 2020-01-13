7ac36b2ff314496a3875d1c0f85946c21e35ae35
Tim Sullivan <tisulliv@microsoft.com>
2020-01-13 13:43:34 -0600

    Added debug statements

--- PowerShell/Set-AzBudgetForRG.ps1
+++ PowerShell/Set-AzBudgetForRG.ps1
@@ -5,9 +5,12 @@ param (
     $ResourceGroupName
 )
 
-$billing_periods = Get-AzBillingPeriod | sort-object -Top 5 | Select-Object name
+$billing_periods = Get-AzBillingPeriod | sort-object -Top 5 | Select-Object -ExpandProperty name
+Write-Debug $billing_periods
 
 foreach ($period in $billing_periods) {
-    Get-AzConsumptionUsageDetail -ResourceGroup $ResourceGroupName -BillingPeriodName $period | Measure-Object PretaxCost -Sum
+    Write-Debug $period
+    $summed_cost = Get-AzConsumptionUsageDetail -ResourceGroup $ResourceGroupName -BillingPeriodName $period | Measure-Object PretaxCost -Sum | Select-Object -ExpandProperty Sum
+    Write-Debug $summed_cost
 }
     
