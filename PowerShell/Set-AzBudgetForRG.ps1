dc1b03b05c0236a384cb1e849978f01270c8dffa
Tim Sullivan <tisulliv@microsoft.com>
2020-01-13 14:02:26 -0600

    Added logic for summed cost

--- PowerShell/Set-AzBudgetForRG.ps1
+++ PowerShell/Set-AzBudgetForRG.ps1
@@ -7,9 +7,13 @@ param (
 
 $billing_periods = Get-AzBillingPeriod | sort-object -Top 5 | Where-Object BillingPeriodEndDate -lt (get-date) | Select-Object -ExpandProperty name
 
-foreach ($period in $billing_periods) {
+foreach ($period in $billing_periods) 
+{
     Write-Verbose $period
     $summed_cost = Get-AzConsumptionUsageDetail -ResourceGroup $ResourceGroupName -BillingPeriodName $period | Measure-Object PretaxCost -Sum | Select-Object -ExpandProperty Sum
-    Write-Verbose $summed_cost
+    if ($summed_cost) 
+    {
+        Write-Verbose $summed_cost
+    }       
 }
     
