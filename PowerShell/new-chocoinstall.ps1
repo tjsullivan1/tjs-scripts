3b746cb0fc7e1611c2f7f21586536c06abaa77ec
Tim Sullivan <tim@sullivanenterprises.org>
2021-03-11 20:15:26 +0000

    separate choco install

---
+++ PowerShell/new-chocoinstall.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope User -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+sleep 10
