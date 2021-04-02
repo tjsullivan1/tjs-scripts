10afde4186ccf4a60d11b97f593370a0b6a8c7ee
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-04-02 15:19:46 -0500

    Update New-WVDOneDriveInstall.ps1

--- PowerShell/New-WVDOneDriveInstall.ps1
+++ PowerShell/New-WVDOneDriveInstall.ps1
@@ -8,4 +8,4 @@ OneDriveSetup.exe /allusers
 
 REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "SilentAccountConfig" /t REG_DWORD /d 1 /f
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "<tenant-id>" /f
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "012c6d21-cad3-4e61-98dd-2bc660a112b8" /f
