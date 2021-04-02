6e6144aaa4557242a4812ee13eb166c07a1c4a80
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-04-02 15:17:52 -0500

    Create New-WVDOneDriveInstall.ps1

---
+++ PowerShell/New-WVDOneDriveInstall.ps1
@@ -0,0 +1,11 @@
+invoke-webrequest https://aka.ms/OneDriveWVD-Installer -OutFile OneDriveSetup.exe
+
+OneDriveSetup.exe /uninstall
+
+REG ADD "HKLM\Software\Microsoft\OneDrive" /v "AllUsersInstall" /t REG_DWORD /d 1 /reg:64
+
+OneDriveSetup.exe /allusers 
+
+REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "SilentAccountConfig" /t REG_DWORD /d 1 /f
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "<tenant-id>" /f
