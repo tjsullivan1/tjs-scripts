193d9146d2c2752642c14344a3e254a20cf6ea97
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-04-21 12:57:07 -0500

    Update New-WVDOneDriveInstall.ps1
    fixed the path and the reg add

--- PowerShell/New-WVDOneDriveInstall.ps1
+++ PowerShell/New-WVDOneDriveInstall.ps1
@@ -1,10 +1,10 @@
 invoke-webrequest https://aka.ms/OneDriveWVD-Installer -OutFile OneDriveSetup.exe
 
-OneDriveSetup.exe /uninstall /silent
+.\OneDriveSetup.exe /uninstall /silent
 
-REG ADD "HKLM\Software\Microsoft\OneDrive" /v "AllUsersInstall" /t REG_DWORD /d 1 /reg:64
+REG ADD "HKLM\Software\Microsoft\OneDrive" /v "AllUsersInstall" /t REG_DWORD /d 1 /reg:64 /f
 
-OneDriveSetup.exe /allusers /silent
+.\OneDriveSetup.exe /allusers /silent
 
 REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "SilentAccountConfig" /t REG_DWORD /d 1 /f
