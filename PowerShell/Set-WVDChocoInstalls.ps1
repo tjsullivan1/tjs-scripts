45891c1d78c603c6d04550861a950ef0668f537c
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-05-03 10:14:49 -0500

    updated chocolatey to not show download progress.

--- PowerShell/Set-WVDChocoInstalls.ps1
+++ PowerShell/Set-WVDChocoInstalls.ps1
@@ -1,55 +1,55 @@
 choco feature enable -n allowGlobalConfirmation
 
 # language support / runtimes
-choco install python
+choco install --no-progress python
 $date = get-date
 write-host "$date -- Installed  python"
-choco install powershell-core
+choco install --no-progress powershell-core
 $date = get-date
 write-host "$date -- Installed  powershell-core"
-choco install nodejs
+choco install --no-progress nodejs
 $date = get-date
 write-host "$date -- Installed  nodejs"
-choco install azure-cli
+choco install --no-progress azure-cli
 $date = get-date
 write-host "$date -- Installed  azure-cli"
-choco install az.powershell
+choco install --no-progress az.powershell
 $date = get-date
 write-host "$date -- Installed  az.powershell"
-choco install bicep
+choco install --no-progress bicep
 $date = get-date
 write-host "$date -- Installed  bicep"
-choco install terraform
+choco install --no-progress terraform
 $date = get-date
 write-host "$date -- Installed  terraform"
 
 # Dev Tools
-choco install git.install
+choco install --no-progress git.install
 $date = get-date
 write-host "$date -- Installed  git.install"
-choco install gh
+choco install --no-progress gh
 $date = get-date
 write-host "$date -- Installed  gh"
-choco install postman # This install did not seem to work properly for me.
+choco install --no-progress postman # This install did not seem to work properly for me.
 $date = get-date
 write-host "$date -- Installed  postman # This install did not seem to work properly for me."
-choco install azure-functions-core-tools-3
+choco install --no-progress azure-functions-core-tools-3
 $date = get-date
 write-host "$date -- Installed  azure-functions-core-tools-3"
-choco install pycharm
+choco install --no-progress pycharm
 $date = get-date
 write-host "$date -- Installed  pycharm"
-choco install vscode
+choco install --no-progress vscode
 $date = get-date
 write-host "$date -- Installed  vscode"
 
 # Various tools
-choco install gnucash
+choco install --no-progress gnucash
 $date = get-date
 write-host "$date -- Installed  gnucash"
-choco install microsoft-windows-terminal # This install did not seem to work properly for me.
+choco install --no-progress microsoft-windows-terminal # This install did not seem to work properly for me.
 $date = get-date
 write-host "$date -- Installed  microsoft-windows-terminal # This install did not seem to work properly for me."
-choco install microsoft-edge-insider-dev
+choco install --no-progress microsoft-edge-insider-dev
 $date = get-date
 write-host "$date -- Installed  microsoft-edge-insider-dev"
