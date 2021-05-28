db67c9c2348ea150517a5e2cd2d907a5ba9df81c
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-05-28 14:11:58 -0500

    removing aliases from sastoken script

--- PowerShell/New-SasTokenForImageLogs.ps1
+++ PowerShell/New-SasTokenForImageLogs.ps1
@@ -8,11 +8,11 @@ param (
 $sub = az account show --query id -o tsv
 
 # Use the REST endpoint to get created time, PowerShell cmdlet does not show that.
-$rgs = az rest --method get --url "https://management.azure.com/subscriptions/$sub/resourcegroups?api-version=2019-08-01&%24expand=createdTime" | ConvertFrom-Json | select -ExpandProperty value
-$rg = $rgs | where Name -like IT* | Sort-Object -descending createdTime | select -First 1 -ExpandProperty Name
+$rgs = az rest --method get --url "https://management.azure.com/subscriptions/$sub/resourcegroups?api-version=2019-08-01&%24expand=createdTime" | ConvertFrom-Json | Select-Object -ExpandProperty value
+$rg = $rgs | Where-Object Name -like IT* | Sort-Object -descending createdTime | Select-Object -First 1 -ExpandProperty Name
 
 $context = (Get-AzStorageAccount -ResourceGroupName $rg).context
-$blobName = Get-AzStorageBlob -Container $containerName -Context $context | select -ExpandProperty Name
+$blobName = Get-AzStorageBlob -Container $containerName -Context $context | Select-Object -ExpandProperty Name
 
 $token = New-AzStorageBlobSASToken -Blob $blobName -Container $containerName -StartTime $StartTime -ExpiryTime $EndTime -Permission r -Context $context
 
