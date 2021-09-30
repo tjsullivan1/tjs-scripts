f09360fc152ae2bb3877ba93eb672b2714468dc0
Tim Sullivan (CSA) <tisulliv@microsoft.com>
2021-09-30 08:03:36 -0500

    updated monitoring

--- terraform/monitoring-samples/main.tf
+++ terraform/monitoring-samples/main.tf
@@ -37,3 +37,36 @@ resource "azurerm_monitor_action_group" "mag" {
   }
 
 }
+
+resource "azurerm_monitor_metric_alert" "k8spods" {
+  name                = "k8s-pod-restart-metricalert"
+  resource_group_name = azurerm_resource_group.monitor.name
+  scopes              = ["/subscriptions/8b63fe10-d76a-4f8f-81ce-7a5a8b911779/resourceGroups/rg-handsonk8s/providers/Microsoft.ContainerService/managedClusters/aks-tjs-7cpgzar4jatom"]
+  description         = "Action will be triggered when restart count is greater than 0."
+
+  criteria {
+    metric_namespace = "Insights.Container/pods"
+    metric_name      = "restartingContainerCount"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 0
+    skip_metric_validation = true
+
+    dimension {
+      name     = "kubernetes namespace"
+      operator = "Include"
+      values   = ["*"]
+    }
+
+    
+    dimension {
+      name     = "controllerName"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.mag.id
+  }
+}
