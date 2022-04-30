6c5ec92d7895d10b096a0450bf362cddc7a5984b
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-04-30 15:36:13 -0500

    reformatted rss_parser with black

--- python/rss_parser.py
+++ python/rss_parser.py
@@ -1,18 +1,28 @@
-import feedparser
+import csv
 import datetime
 from time import mktime
-import csv
+
+import feedparser
 
 start_date = datetime.datetime.utcnow() - datetime.timedelta(30)
 
-d = feedparser.parse('https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/atom')
+d = feedparser.parse(
+    "https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/c3a1be8a-50db-47b7-d5eb-259debc3abcc/atom"
+)
 
-with open('test.csv', 'w', newline='') as csvfile:
-    fieldnames = ['title', 'link', 'summary', 'updated']
+with open("test.csv", "w", newline="") as csvfile:
+    fieldnames = ["title", "link", "summary", "updated"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     for entry in d.entries:
         entry_date = entry.updated_parsed
         dt = datetime.datetime.fromtimestamp(mktime(entry_date))
         if dt > start_date:
-            writer.writerow({'title': entry.title, 'link': entry.link, 'summary': entry.summary, 'updated': entry.updated})
+            writer.writerow(
+                {
+                    "title": entry.title,
+                    "link": entry.link,
+                    "summary": entry.summary,
+                    "updated": entry.updated,
+                }
+            )
