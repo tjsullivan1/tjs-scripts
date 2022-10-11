1a280d0933c5ca01c4e70edef38922acbb873a2b
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-10-11 11:07:54 -0500

    updated variables to include location

--- terraform/preso_storage_account_repeatable/vars.tf
+++ terraform/preso_storage_account_repeatable/vars.tf
@@ -2,3 +2,9 @@ variable "disambiguation" {
     type = string
     description = "Provides an indication of what this particular storage account is for."
 }
+
+variable "location" {
+  type = string
+  description = "Azure Region"
+  default = "eastus"
+}
