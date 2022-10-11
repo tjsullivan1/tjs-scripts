6d79a314cd2c21ab59d6ed847b8445ac9e5fe241
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-10-11 11:05:19 -0500

    updated module to take an account name

--- terraform/modules/storage_account/main.tf
+++ terraform/modules/storage_account/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "tjs" {
-  name                     = "sa${lower(var.disambiguation)}${var.random_string}"
+  name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
--- terraform/modules/storage_account/vars.tf
+++ terraform/modules/storage_account/vars.tf
@@ -1,22 +1,12 @@
-variable "disambiguation" {
-  type        = string
-  description = "Provides an indication of what this particular storage account is for."
-
-  validation {
-    condition = length(var.disambiguation) < 15
-    error_message = "Please reduce the length of your disambiguation string."
-  }
-}
-
 variable "location" {
   type = string
   description = "Azure Region"
   default = "eastus"
 }
 
-variable "random_string" {
+variable "storage_account_name" {
   type = string
-  description = "a randomly generated string"
+  description = "a name for the storage account"
 }
 
 variable "resource_group_name" {
