9505de9e90207a8f8da5a1d82d145d3c1235dd11
Tim Sullivan <timothyj.sullivan1@gmail.com>
2023-01-24 10:56:30 -0600

    Update centos7-update.sh

--- bash/centos7-update.sh
+++ bash/centos7-update.sh
@@ -5,3 +5,17 @@ ls /dev/disk
 
 # Update CentOS 7
 sudo yum -y update
+
+# Import MSFT Repo Key
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Add the azure-cli repo
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+
+# Install az cli
+sudo dnf install azure-cli
