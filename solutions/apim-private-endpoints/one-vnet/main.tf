a11291000e411a1488082e3ebe31eeb6ec9e868d
Tim Sullivan <timothyj.sullivan1@gmail.com>
2023-02-09 12:18:32 -0600

    added vm

--- solutions/apim-private-endpoints/one-vnet/main.tf
+++ solutions/apim-private-endpoints/one-vnet/main.tf
@@ -98,3 +98,50 @@ resource "azurerm_subnet" "function2" {
     virtual_network_name = azurerm_virtual_network.vnetapim.name
     address_prefixes     = ["10.100.3.0/24"]
 }
+
+resource "azurerm_subnet" "vms" {
+    name                 = "snet-${var.disambiguation}-${random_string.suffix.result}-vms"
+    resource_group_name  = azurerm_resource_group.vnetapim.name
+    virtual_network_name = azurerm_virtual_network.vnetapim.name
+    address_prefixes     = ["10.100.4.0/24"]
+}
+
+resource "azurerm_network_interface" "tester" {
+  name                = "tester-nic"
+  location            = azurerm_resource_group.vnetapim.location
+  resource_group_name = azurerm_resource_group.vnetapim.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vms.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "tester" {
+  name                = "tester"
+  resource_group_name = azurerm_resource_group.vnetapim.name
+  location            = azurerm_resource_group.vnetapim.location
+  size                = "Standard_B4ms"
+  admin_username      = "tjs"
+  network_interface_ids = [
+    azurerm_network_interface.tester.id,
+  ]
+
+  admin_ssh_key {
+    username   = "tjs"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+}
