51636384c57b30d3eb745df84458d58ded1ed472
Tim Sullivan <timothyj.sullivan1@gmail.com>
2023-08-17 22:55:00 -0500

    example without peering the vnets. Still requires the same process as the parent folder

---
+++ terraform/private_acr_across_subs/non-peered/providers.tf
@@ -0,0 +1,34 @@
+# providers.tf
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+provider "azurerm" {
+
+  subscription_id = var.primary_subscription_id
+  client_id       = var.primary_subscription_client_id
+  client_secret   = var.primary_subscription_client_secret
+  tenant_id       = var.primary_subscription_tenant_id
+
+  features {}
+}
+
+provider "azurerm" {
+  alias = "secondary"
+
+  subscription_id = var.secondary_subscription_id
+  client_id       = var.secondary_subscription_client_id
+  client_secret   = var.secondary_subscription_client_secret
+  tenant_id       = var.secondary_subscription_tenant_id
+  
+  features {}
+}
