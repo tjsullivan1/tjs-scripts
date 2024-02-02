e7e65c21820800125c3290667d41b31bfd76ea87
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-02-02 16:22:28 -0600

    Working sample locally, about to push to dockerhub

---
+++ python/identitytests/main.py
@@ -0,0 +1,22 @@
+import os
+from azure.identity import DefaultAzureCredential
+from dotenv import load_dotenv
+
+
+# This script requires four environment variables to be set, either in a .env or actually in the environment to run LOCALLY
+# AZURE_CLIENT_ID
+# AZURE_CLIENT_SECRET
+# AZURE_TENANT_ID
+# SCOPE
+# If running on K8s or in Azure, ONLY SCOPE is needed. The others can use a managed identity.
+if ( os.environ['ENVIRONMENT'] == 'development'):
+    print("Loading environment variables from .env file")
+    load_dotenv("dev.env")  # take environment variables from .env.
+
+credential = DefaultAzureCredential()
+
+scope = os.getenv("SCOPE")
+token = credential.get_token(scope)
+
+print(token.token)
+
