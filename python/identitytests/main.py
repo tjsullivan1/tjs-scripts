bf5118d6bd924b3b4ec46f5a9f3c610bf34340ea
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-03-21 14:03:11 -0500

    identity test updated to use os.getenv instead of dict reference

--- python/identitytests/main.py
+++ python/identitytests/main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 # AZURE_TENANT_ID
 # SCOPE
 # If running on K8s or in Azure, ONLY SCOPE is needed. The others can use a managed identity.
-if ( os.environ['ENVIRONMENT'] == 'development'):
+if ( os.getenv('ENVIRONMENT')== 'development'):
     print("Loading environment variables from .env file")
     load_dotenv("dev.env")  # take environment variables from .env.
 
