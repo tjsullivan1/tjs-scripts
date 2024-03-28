b4d4b459fc9fbd64615c57af0f92893f74f56059
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-03-28 16:09:09 -0500

    updated with sample http files properly working with environment vars and vscode settings

--- solutions/apim-parameterized-samples/backend-api/function_app.py
+++ solutions/apim-parameterized-samples/backend-api/function_app.py
@@ -5,7 +5,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="HttpExample", methods=("GET","POST"), auth_level=func.AuthLevel.ANONYMOUS)
 def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
