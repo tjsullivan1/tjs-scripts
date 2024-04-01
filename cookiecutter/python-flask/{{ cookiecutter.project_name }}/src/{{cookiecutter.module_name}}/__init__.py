a3bf07f6e803b223cf90c4b6a8dc2324dff03907
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-04-01 14:39:53 -0500

    cleaning up flask cookiecutter

--- cookiecutter/python-flask/{{ cookiecutter.project_name }}/src/{{cookiecutter.module_name}}/__init__.py
+++
@@ -1,6 +0,0 @@
-"""{{ cookiecutter.project_name }}
-By {{ cookiecutter.author_name }} {{ cookiecutter.author_email }}
-
-{{ cookiecutter.project_short_description }}"""
-
-__version__ = '{{cookiecutter.project_version}}'
--- cookiecutter/python-flask/{{ cookiecutter.project_name }}/src/{{cookiecutter.module_name}}/__main__.py
+++
@@ -1,4 +0,0 @@
-import {{ cookiecutter.module_name }}
-
-if __name__ == '__main__':
-    pass
