53025ee3b192430d7c4cc296228b0c9c6ad1d87a
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-04-05 14:55:11 -0500

    added render template

--- cookiecutter/python-flask/{{ cookiecutter.project_name }}/src/app.py
+++ cookiecutter/python-flask/{{ cookiecutter.project_name }}/src/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html", page_name="Home")
 
 
 if __name__ == "__main__":
