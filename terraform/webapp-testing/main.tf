fb3aad3667be7e8bbecf5bb60e84d4289a4efca3
Tim Sullivan <timothyj.sullivan1@gmail.com>
2025-10-27 16:58:18 +0000

    missing quote

--- terraform/webapp-testing/main.tf
+++ terraform/webapp-testing/main.tf
@@ -4,7 +4,7 @@ data "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_service_plan" "main" {
-  name                = "asp-"${var.web_app_name_prefix}"
+  name                = "asp-${var.web_app_name_prefix}"
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Linux"
